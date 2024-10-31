Fixed error with normalizing the residual

neural_lam/utils.py
@@ -248,9 +248,7 @@ class MLP(nn.Module):
                 layers.append(nn.SiLU())  # Swish activation
 
         self.mlp_layers = nn.Sequential(*layers)
-        
-            
-                
+         
         # Optionally add layer norm to output
         if layer_norm:
             # self.layer_norm = (nn.LayerNorm(blueprint[-1]))

neural_lam/weather_dataset.py
@@ -53,7 +53,7 @@ class WeatherDataset(torch.utils.data.Dataset):
         # Now on form "yyymmddhh_mbrXXX"
 
         if subset:
-            self.sample_names = self.sample_names[:50]  # Limit to 50 samples
+            self.sample_names = self.sample_names[:1]  # Limit to 50 samples, changed to 1
 
         self.sample_length = pred_length + 2  # 2 init states
         self.subsample_step = subsample_step