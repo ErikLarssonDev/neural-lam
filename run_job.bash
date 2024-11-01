64ec0426c588554d1c7249595ed89e83de3399b3
ErikLarssonDev <erik.u.larsson1@gmail.com>
2024-11-01 14:21:23 +0100

    Ran eval for edm precond network

--- run_job.bash
+++ run_job.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J edm_state
+#SBATCH -J edm_state_heun
 #SBATCH -t 3-00:00:00
 #SBATCH --gpus=1 -C "thin"
 #SBATCH --mail-type=ALL
@@ -18,16 +18,20 @@ PYTHON_SCRIPT_PATH="neural_lam.train_model"
 
 MODEL="diffusion" # N_O, WNO2d, diffusion
 DIFFUSION_MODEL="--diffusion_model edm --graph hierarchical"
-RUN_NAME="--wandb_run_name edm_state"
+RUN_NAME="--wandb_run_name edm_state_heun"
 PATH_TO_MODEL="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/diff_graph_fm_hier-diffusion-4x64-09_16_13-7088/last.ckpt"
 DIFFUSION_MODEL_400e="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/diff_graph_fm_hier_400e-diffusion-4x64-09_23_09-3756/last.ckpt"
 DIFFUSION_GRAPH_FM_CON_NORM_200e="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/diff_con_layer_norm_res-diffusion-4x64-10_03_10-5623/last.ckpt"
 LOSS_DIFF_GRAPH_CON_NORM_RES="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/loss_res_graph_fm_con_norm_no_border-diffusion-4x64-10_15_11-6256/last.ckpt"
 LOSS_DIFF_EDM_CON_NORM_RES="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/loss_diff_edm_no_border-diffusion-4x64-10_15_10-5635/last.ckpt"
 NORM_LOSS_DIFF_GRAPH_CON_NORM_RES="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/norm_res-diffusion-4x64-10_29_14-5875/last.ckpt"
+NORM_LOSS_DIFF_EDM="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/edm_res-diffusion-6x128-10_31_09-3448/last.ckpt"
+NORM_LOSS_STATE_EDM="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/edm_state-diffusion-6x128-10_31_09-9003/last.ckpt"
+NORM_LOSS_RES_GRAPH_FM=""
+NORM_LOSS_STATE_GRAPH_FM=""
 
 # Execute Python script with arguments
-python3 train_model.py "--model" $MODEL $DIFFUSION_MODEL "--n_workers" 16 $RUN_NAME --batch_size 4 --val_interval 10 # --pred_residual # --load $NORM_LOSS_DIFF_GRAPH_CON_NORM_RES --eval test --subset_ds 1 --sampler heun
+python3 train_model.py "--model" $MODEL $DIFFUSION_MODEL "--n_workers" 16 $RUN_NAME --batch_size 4 --val_interval 10 --load $NORM_LOSS_STATE_EDM --eval test --subset_ds 1 --sampler heun
 
 # python3 train_model.py --model diffusion --diffusion_model graph_fm --graph hierarchical --pred_residual --batch_size 10 --subset_ds 1 --eval test  --sampler heun --load /proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/loss_res_graph_fm_con_norm_no_border-diffusion-4x64-10_15_11-6256/last.ckpt 
 # python3 train_model.py --model diffusion --diffusion_model edm --pred_residual --batch_size 4 --wandb_run_name std_1_loss_diff_edm_no_border --val_interval 10 --n_workers 16 # --eval val --load /proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/loss_diff_edm_no_border-diffusion-4x64-10_15_10-5635/last.ckpt
