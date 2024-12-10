d747390feb4825480a632f66db1acbbad53dd880
ErikLarssonDev <erik.u.larsson1@gmail.com>
2024-12-10 08:36:47 +0100

    Ran eval of border diffusion model

--- run_job.bash
+++ run_job.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J cosine_graph_fm_border_condition_400e
+#SBATCH -J BORDER_GRAPH_FM_400e
 #SBATCH -t 3-00:00:00
 #SBATCH --gpus=1 -C "thin"
 #SBATCH --mail-type=ALL
@@ -18,7 +18,7 @@ PYTHON_SCRIPT_PATH="neural_lam.train_model"
 
 MODEL="diffusion" # N_O, WNO2d, diffusion
 DIFFUSION_MODEL="--diffusion_model graph_fm --graph hierarchical"
-RUN_NAME="--wandb_run_name cosine_graph_fm_border_condition_400e"
+RUN_NAME="--wandb_run_name BORDER_GRAPH_FM_400e"
 
 # Paths to saved models
 PATH_TO_MODEL="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/diff_graph_fm_hier-diffusion-4x64-09_16_13-7088/last.ckpt"
@@ -36,9 +36,11 @@ NORM_LOSS_RES_GRAPH_FM_SIGMA_0002="/proj/berzelius-2022-164/users/x_erila/neural
 NORM_LOSS_RES_GRAPH_FM_400e="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/graph_fm_sigmamin_0.0002-diffusion-6x128-11_08_10-2170/last.ckpt"
 BORDER_GRAPH_FM="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/graph_fm_border_condition-diffusion-6x128-12_02_14-8805/last.ckpt"
 BORDER_GRAPH_FM_300e="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/graph_fm_border_condition_300e-diffusion-6x128-12_06_07-7658/last.ckpt"
+COSINE_BORDER_GRAPH_FM_400e="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/cosine_graph_fm_border_condition_400e-diffusion-6x128-12_09_07-5373/last.ckpt"
+BORDER_GRAPH_FM_400e="/proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/graph_fm_border_condition_400e-diffusion-6x128-12_09_07-5907/last.ckpt"
 
 # Execute Python script with arguments
-python3 neural_lam/train_model.py "--model" $MODEL $DIFFUSION_MODEL "--n_workers" 16 $RUN_NAME --batch_size 4 --val_interval 10 --pred_residual --border_condition --lr 0.00001 --epochs 400 --load $BORDER_GRAPH_FM_300e --lr_scheduler cosine #   # --eval test --sampler heun # --plot_diffusion_steps
+python3 neural_lam/train_model.py "--model" $MODEL $DIFFUSION_MODEL "--n_workers" 16 $RUN_NAME --batch_size 4 --val_interval 10 --pred_residual --border_condition --load $BORDER_GRAPH_FM_400e --eval test # --lr 0.00001 --epochs 400 --load $BORDER_GRAPH_FM_300e --lr_scheduler cosine #   # --eval test --sampler heun # --plot_diffusion_steps
 
 # python3 neural_lam/train_model.py --model diffusion --diffusion_model graph_fm --graph hierarchical --pred_residual --batch_size 10 --border_condition --subset_ds --lr_scheduler cosine --epochs 300 --load /proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/graph_fm_border_condition-diffusion-6x128-12_02_14-8805/last.ckpt
 # python3 train_model.py --model diffusion --diffusion_model edm --pred_residual --batch_size 4 --wandb_run_name std_1_loss_diff_edm_no_border --val_interval 10 --n_workers 16 # --eval val --load /proj/berzelius-2022-164/users/x_erila/neural-lam/saved_models/loss_diff_edm_no_border-diffusion-4x64-10_15_10-5635/last.ckpt
