31fd422c917b431afa3109794728a04848c4cdfe
ErikLarssonDev <erik.u.larsson1@gmail.com>
2025-02-18 09:04:25 +0100

    Code clean-up and fixed command line arguments for saving a sample during inference

--- neural_lam/weather_dataset.py
+++ neural_lam/weather_dataset.py
@@ -285,7 +285,6 @@ class WeatherDataset(torch.utils.data.Dataset):
         # boundary, boundary forcing should contain (in order) prev_state,
         # prev_prev_state, forcing (and later added on static features).
 
-        # TODO: Add the option to condition on the boundary forcing for the next time step boundary_forcing_sample[2:],
         boundary_forcing = torch.cat(
             (
                 boundary_forcing_sample[1:-1], # prev_state
