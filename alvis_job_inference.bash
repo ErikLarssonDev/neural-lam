a36eae00bf1066dd204576087c13ac7df4248e4d
Mikhail Ivanov <mikhail.ivanov.1995@gmail.com>
2025-07-25 21:26:13 +0200

    small update to the config and the slurm script

--- alvis_job_inference.bash
+++ alvis_job_inference.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J SI
 #SBATCH -A NAISS2024-22-955 -p alvis
-#SBATCH -N 1 --gpus-per-node=A40:2
-#SBATCH -t 0-04:00:00
-#SBATCH --output=2A40.out
+#SBATCH -N 1 --gpus-per-node=A40:4
+#SBATCH -t 1-00:00:00
+#SBATCH --output=exp8.out
 
 export HDF5_USE_FILE_LOCKING=FALSE
 
@@ -12,14 +12,19 @@ RUN_NAME="--wandb_run_name SI_50e"
 # Switch to the correct directory
 cd /mimer/NOBACKUP/groups/mlhighres/users/mikhaili/neural-lam
 
-# Activate wandb
-apptainer exec ~/neural-lam.sif wandb off
+output_path="output/250725/exp8"
+ensemble_size=25
+sampler_steps=100
+sampler="euler"
+
+mkdir -p $output_path
+cp alvis_job_inference.bash $output_path
 
 # Saved models
 SI_50e="/mimer/NOBACKUP/groups/mlhighres/users/erifh/neural-lam/saved_models/SI_50e-SI-6x128-07_10_12-7283/last.ckpt"
 
 # Define the local repository path
-REPO_PATH=/mimer/NOBACKUP/groups/mlhighres/users/mikhaili/neural-lam
+REPO_PATH="/mimer/NOBACKUP/groups/mlhighres/users/mikhaili/neural-lam"
 
 # Run the container
 apptainer exec \
@@ -29,13 +34,13 @@ apptainer exec \
   --model SI \
   --data_config neural_lam/clim_config_inference.yaml \
   --diffusion_model song_unet \
-  --output_path output/scaling-tests/2gpu/pt \
+  --output_path $output_path \
   --n_workers 16 \
   --eval test \
   --n_example_pred 99999 \
   --batch_size 1 \
-  --ensemble_size 16 \
-  --sampler_steps 10 \
+  --ensemble_size $ensemble_size \
+  --sampler_steps $sampler_steps \
   --load $SI_50e \
-  --sampler euler_2 \
+  --sampler $sampler \
   --save_output
